496461007de3191c34951a402663d46754b68626
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-05-07 01:37:36 +0530

    Add cart item counter in navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,24 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Label } from "semantic-ui-react";
+import { useSelector } from "react-redux";
 import "./Navigation.css";
 
-const Navigation = () => (
-  <header className="main-navigation">
-    <div className="main-navigation-logo">
-      <h1>Shopiee</h1>
-    </div>
-    <nav className="main-navigation-item">
-      <ul>
-        <li>
-          <NavLink to="/">Products</NavLink>
-        </li>
+const Navigation = () => {
+  const cart = useSelector((state) => state.products.cart);
 
-        <li>
-          <NavLink to="/cart">Cart</NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+  return (
+    <header className="main-navigation">
+      <div className="main-navigation-logo">
+        <h1>Shopiee</h1>
+      </div>
+      <nav className="main-navigation-item">
+        <ul>
+          <li>
+            <NavLink to="/">Products</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/cart">Cart</NavLink>
+            <Label color="white" size="mini">
+              {cart.length}
+            </Label>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export { Navigation };
