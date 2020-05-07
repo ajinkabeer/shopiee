b25f48fed88c7f637b47130efddbf0b251e43c0f
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-05-07 15:29:53 +0530

    Updates

---
+++ precache-manifest.0eb464db47076a7ac9dc2fe50df71476.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e4268ea2d43b1412de4c9a6b4a4a96",
+    "url": "/shopiee/index.html"
+  },
+  {
+    "revision": "da4f08b17e0f4e4a040b",
+    "url": "/shopiee/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "4de7f09a4e2af5e0e682",
+    "url": "/shopiee/static/css/main.76a3d61b.chunk.css"
+  },
+  {
+    "revision": "da4f08b17e0f4e4a040b",
+    "url": "/shopiee/static/js/2.ca8e288a.chunk.js"
+  },
+  {
+    "revision": "e01755ceac4b87e9abc519b2042ab467",
+    "url": "/shopiee/static/js/2.ca8e288a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de7f09a4e2af5e0e682",
+    "url": "/shopiee/static/js/main.3cfa54be.chunk.js"
+  },
+  {
+    "revision": "ce046ef448927a26f97c",
+    "url": "/shopiee/static/js/runtime-main.ec81f299.js"
+  }
+]);
