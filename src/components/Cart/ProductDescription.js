8992f9641c7df58e81d57f23365f2d4b1fb12a55
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-05-15 00:29:01 +0530

    Adjust cart li margin

--- src/components/Cart/ProductDescription.js
+++ src/components/Cart/ProductDescription.js
@@ -1,31 +1,33 @@
 import React from "react";
-import { List, Button } from "semantic-ui-react";
+import { List, Button, Segment } from "semantic-ui-react";
 import { useSelector } from "react-redux";
 
 const ProductDescription = ({ onButtonClick }) => {
   const totalAmount = useSelector((state) => state.products.amount);
   return (
-    <List divided verticalAlign="middle">
-      <List.Item>
-        <List.Content floated="right">Rs. {totalAmount}</List.Content>
-        <List.Content>Total Amount</List.Content>
-      </List.Item>
-      <List.Item>
-        <List.Content floated="right">
-          Rs. {Math.floor(Math.random() * 100) + 1}
-        </List.Content>
-        <List.Content>Eligible Discount</List.Content>
-      </List.Item>
+    <Segment>
+      <List verticalAlign="middle">
+        <List.Item style={{ marginTop: 10 }}>
+          <List.Content floated="right">Rs. {totalAmount}</List.Content>
+          <List.Content>Total Amount</List.Content>
+        </List.Item>
+        <List.Item style={{ marginTop: 10, marginBottom: 10 }}>
+          <List.Content floated="right">
+            Rs. {Math.floor(Math.random() * 100) + 1}
+          </List.Content>
+          <List.Content>Eligible Discount</List.Content>
+        </List.Item>
 
-      <Button
-        positive
-        fluid
-        onClick={onButtonClick}
-        disabled={totalAmount === 0 ? true : false}
-      >
-        Checkout
-      </Button>
-    </List>
+        <Button
+          positive
+          fluid
+          onClick={onButtonClick}
+          disabled={totalAmount === 0 ? true : false}
+        >
+          Checkout
+        </Button>
+      </List>
+    </Segment>
   );
 };
 
