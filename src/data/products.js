5c9aff3c008d7d355ff755d410b65ed2faa544ae
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-05-15 12:57:35 +0530

    Add new images links froms s3 bucket

--- src/data/products.js
+++ src/data/products.js
@@ -7,10 +7,10 @@ export const data = [
     price: 455,
     quantity: 1,
     photos: [
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11054736/2019/12/13/ca2fcf06-a147-40bf-8f5b-2f64e473e8481576221535837-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-1691576221533577-1.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11054736/2019/12/13/9b106b67-1e3c-4e6d-b7d0-a9a126c536491576221535743-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-1691576221533577-3.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11054736/2019/12/13/4a1d6cad-7f50-42c8-862a-5b4160f75dd51576221535629-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-1691576221533577-5.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11054736/2019/12/13/ca2fcf06-a147-40bf-8f5b-2f64e473e8481576221535837-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-1691576221533577-1.jpg",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/107386c0-6081-4c61-9898-f9adabf9d2061529385860750-HRX-by-Hrithik-Roshan-Men-Charcoal-Grey-Slim-Advanced-Rapid--7.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/02e0a549-8eb9-4fe9-b934-6defd31ac7291548937957899-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-1961548937956898-4.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/4c3260fa-d085-4f00-a285-f1acd88b43311571123456811-GAP-Mens-All-Day-Pique-Polo-Shirt-7361571123455295-4.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/693d0cf6-0c4d-4f97-a399-1284c7abb1d81529385860848-HRX-by-Hrithik-Roshan-Men-Charcoal-Grey-Slim-Advanced-Rapid--2.webp",
     ],
   },
   {
@@ -21,10 +21,10 @@ export const data = [
     price: 580,
     quantity: 1,
     photos: [
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10236397/2019/10/15/dc490212-3d6d-414c-8194-9f9f10ad105d1571123456993-GAP-Mens-All-Day-Pique-Polo-Shirt-7361571123455295-1.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10236397/2019/10/15/dc490212-3d6d-414c-8194-9f9f10ad105d1571123456993-GAP-Mens-All-Day-Pique-Polo-Shirt-7361571123455295-1.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10236397/2019/10/15/4c3260fa-d085-4f00-a285-f1acd88b43311571123456811-GAP-Mens-All-Day-Pique-Polo-Shirt-7361571123455295-4.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10236397/2019/10/15/d5c6461b-4c40-4270-a1ad-c147c24f70191571123456764-GAP-Mens-All-Day-Pique-Polo-Shirt-7361571123455295-5.jpg",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/10909a08-74d3-47aa-9099-77faf0d6878f1564575072993-Levis-Men-Tshirts-3111564575071505-3.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/dc490212-3d6d-414c-8194-9f9f10ad105d1571123456993-GAP-Mens-All-Day-Pique-Polo-Shirt-7361571123455295-1.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/10909a08-74d3-47aa-9099-77faf0d6878f1564575072993-Levis-Men-Tshirts-3111564575071505-3.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/dc490212-3d6d-414c-8194-9f9f10ad105d1571123456993-GAP-Mens-All-Day-Pique-Polo-Shirt-7361571123455295-1.webp",
     ],
   },
   {
@@ -36,10 +36,10 @@ export const data = [
     price: 365,
     quantity: 1,
     photos: [
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9911447/2019/7/31/840ab193-5f4f-4aed-b482-a989c8d9fb2e1564575073033-Levis-Men-Tshirts-3111564575071505-1.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9911447/2019/7/31/10909a08-74d3-47aa-9099-77faf0d6878f1564575072993-Levis-Men-Tshirts-3111564575071505-3.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9911447/2019/7/31/fa949f97-f594-4d2a-9b76-50d4bc772e2f1564575072974-Levis-Men-Tshirts-3111564575071505-4.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9911447/2019/7/31/840ab193-5f4f-4aed-b482-a989c8d9fb2e1564575073033-Levis-Men-Tshirts-3111564575071505-1.jpg",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/11523254006898-HRX-by-Hrithik-Roshan-Men-Navy-Blue-Solid-Round-Neck-T-shirt-4811523254006766-5.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/11523254006973-HRX-by-Hrithik-Roshan-Men-Navy-Blue-Solid-Round-Neck-T-shirt-4811523254006766-1.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/fa949f97-f594-4d2a-9b76-50d4bc772e2f1564575072974-Levis-Men-Tshirts-3111564575071505-4.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/11523254006898-HRX-by-Hrithik-Roshan-Men-Navy-Blue-Solid-Round-Neck-T-shirt-4811523254006766-5.webp",
     ],
   },
   {
@@ -51,10 +51,10 @@ export const data = [
     price: 245,
     quantity: 1,
     photos: [
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/8350999/2019/5/6/a5f3db8d-a5f2-49ab-8309-6b4db9c7f3d31557143795343-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-3461557143793976-1.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/8350999/2019/5/6/0d8795eb-6ce5-40d5-b702-c6b2f2cfed3e1557143795310-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-3461557143793976-3.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/8350999/2019/5/6/a5f3db8d-a5f2-49ab-8309-6b4db9c7f3d31557143795343-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-3461557143793976-1.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/8350999/2019/5/6/a5f3db8d-a5f2-49ab-8309-6b4db9c7f3d31557143795343-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-3461557143793976-1.jpg",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/4a1d6cad-7f50-42c8-862a-5b4160f75dd51576221535629-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-1691576221533577-5.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/10909a08-74d3-47aa-9099-77faf0d6878f1564575072993-Levis-Men-Tshirts-3111564575071505-3.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/dc490212-3d6d-414c-8194-9f9f10ad105d1571123456993-GAP-Mens-All-Day-Pique-Polo-Shirt-7361571123455295-1.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/fa949f97-f594-4d2a-9b76-50d4bc772e2f1564575072974-Levis-Men-Tshirts-3111564575071505-4.webp",
     ],
   },
   {
@@ -66,10 +66,11 @@ export const data = [
     price: 545,
     quantity: 1,
     photos: [
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9909435/2019/7/8/effde52b-af6c-4474-8001-12caa39ccd161562570408527-Levis-Men-Tshirts-7271562570407368-1.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9909435/2019/7/8/6fa37981-485a-4462-80f7-f1b7563362de1562570408475-Levis-Men-Tshirts-7271562570407368-4.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9909435/2019/7/8/6e591223-0a5f-44f8-bcfc-fb1cced4813d1562570408463-Levis-Men-Tshirts-7271562570407368-5.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9909435/2019/7/8/effde52b-af6c-4474-8001-12caa39ccd161562570408527-Levis-Men-Tshirts-7271562570407368-1.jpg",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/6fa37981-485a-4462-80f7-f1b7563362de1562570408475-Levis-Men-Tshirts-7271562570407368-4.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/10909a08-74d3-47aa-9099-77faf0d6878f1564575072993-Levis-Men-Tshirts-3111564575071505-3.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/dc490212-3d6d-414c-8194-9f9f10ad105d1571123456993-GAP-Mens-All-Day-Pique-Polo-Shirt-7361571123455295-1.webp",
+
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/fa949f97-f594-4d2a-9b76-50d4bc772e2f1564575072974-Levis-Men-Tshirts-3111564575071505-4.webp",
     ],
   },
   {
@@ -81,10 +82,10 @@ export const data = [
     price: 455,
     quantity: 1,
     photos: [
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/8351093/2019/1/31/826a06de-d9e2-454b-b929-deb64f06c0001548937957940-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-1961548937956898-1.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/8351093/2019/1/31/65c6d246-60c6-4ae7-9122-11a284eb56621548937957913-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-1961548937956898-3.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/8351093/2019/1/31/65c6d246-60c6-4ae7-9122-11a284eb56621548937957913-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-1961548937956898-3.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/8351093/2019/1/31/02e0a549-8eb9-4fe9-b934-6defd31ac7291548937957899-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-1961548937956898-4.jpg",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/6fa37981-485a-4462-80f7-f1b7563362de1562570408475-Levis-Men-Tshirts-7271562570407368-4.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/10909a08-74d3-47aa-9099-77faf0d6878f1564575072993-Levis-Men-Tshirts-3111564575071505-3.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/dc490212-3d6d-414c-8194-9f9f10ad105d1571123456993-GAP-Mens-All-Day-Pique-Polo-Shirt-7361571123455295-1.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/fa949f97-f594-4d2a-9b76-50d4bc772e2f1564575072974-Levis-Men-Tshirts-3111564575071505-4.webp",
     ],
   },
   {
@@ -96,10 +97,10 @@ export const data = [
     price: 580,
     quantity: 1,
     photos: [
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2425411/2018/4/9/11523254006973-HRX-by-Hrithik-Roshan-Men-Navy-Blue-Solid-Round-Neck-T-shirt-4811523254006766-1.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2425411/2018/4/9/11523254006929-HRX-by-Hrithik-Roshan-Men-Navy-Blue-Solid-Round-Neck-T-shirt-4811523254006766-3.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2425411/2018/4/9/11523254006973-HRX-by-Hrithik-Roshan-Men-Navy-Blue-Solid-Round-Neck-T-shirt-4811523254006766-1.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2425411/2018/4/9/11523254006898-HRX-by-Hrithik-Roshan-Men-Navy-Blue-Solid-Round-Neck-T-shirt-4811523254006766-5.jpg",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/65c6d246-60c6-4ae7-9122-11a284eb56621548937957913-AMERICAN-EAGLE-OUTFITTERS-Men-Tshirts-1961548937956898-3+(1).webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/10909a08-74d3-47aa-9099-77faf0d6878f1564575072993-Levis-Men-Tshirts-3111564575071505-3.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/dc490212-3d6d-414c-8194-9f9f10ad105d1571123456993-GAP-Mens-All-Day-Pique-Polo-Shirt-7361571123455295-1.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/fa949f97-f594-4d2a-9b76-50d4bc772e2f1564575072974-Levis-Men-Tshirts-3111564575071505-4.webp",
     ],
   },
   {
@@ -111,10 +112,10 @@ export const data = [
     price: 365,
     quantity: 1,
     photos: [
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2314372/2018/6/19/29e8ddfd-6f5f-43fd-8b71-dfa8ac6cef681529385860869-HRX-by-Hrithik-Roshan-Men-Charcoal-Grey-Slim-Advanced-Rapid--1.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2314372/2018/6/19/693d0cf6-0c4d-4f97-a399-1284c7abb1d81529385860848-HRX-by-Hrithik-Roshan-Men-Charcoal-Grey-Slim-Advanced-Rapid--2.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2314372/2018/6/19/508fed04-0206-4036-afaf-d0493ccffd6c1529385860771-HRX-by-Hrithik-Roshan-Men-Charcoal-Grey-Slim-Advanced-Rapid--6.jpg",
-      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2314372/2018/6/19/107386c0-6081-4c61-9898-f9adabf9d2061529385860750-HRX-by-Hrithik-Roshan-Men-Charcoal-Grey-Slim-Advanced-Rapid--7.jpg",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/6fa37981-485a-4462-80f7-f1b7563362de1562570408475-Levis-Men-Tshirts-7271562570407368-4.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/10909a08-74d3-47aa-9099-77faf0d6878f1564575072993-Levis-Men-Tshirts-3111564575071505-3.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/dc490212-3d6d-414c-8194-9f9f10ad105d1571123456993-GAP-Mens-All-Day-Pique-Polo-Shirt-7361571123455295-1.webp",
+      "https://images-codacious.s3.ap-south-1.amazonaws.com/fa949f97-f594-4d2a-9b76-50d4bc772e2f1564575072974-Levis-Men-Tshirts-3111564575071505-4.webp",
     ],
   },
 ];
