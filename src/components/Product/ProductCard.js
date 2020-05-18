43d5744a0a997b53fd61d7e282f50b954919e892
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-05-18 09:05:16 +0530

    Add react lite toast in home

--- src/components/Product/ProductCard.js
+++ src/components/Product/ProductCard.js
@@ -4,8 +4,12 @@ import { useDispatch } from "react-redux";
 import { addToCart } from "../../redux/actions/products";
 import "./css/card.css";
 
+import { Toast } from "react-lite-toast";
+import "react-lite-toast/dist/index.css";
+
 const ProductCard = ({ id, photos, brand, category, price, quantity }) => {
   const [redirect, setRedirect] = useState(false);
+  const [toast, setToast] = useState(false);
   const dispatch = useDispatch();
 
   const addItemsToCart = () => {
@@ -18,39 +22,50 @@ const ProductCard = ({ id, photos, brand, category, price, quantity }) => {
       quantity,
       size: "small",
     };
+    setToast(true);
     dispatch(addToCart(product));
   };
 
   return (
-    <div className="card">
-      {redirect && (
-        <Redirect
-          to={{
-            pathname: `/details/${id}`,
-            id,
-          }}
+    <>
+      <div className="card">
+        {redirect && (
+          <Redirect
+            to={{
+              pathname: `/details/${id}`,
+              id,
+            }}
+          />
+        )}
+        <img
+          key={id}
+          src={photos[0]}
+          alt={brand}
+          onClick={() => setRedirect(true)}
+        />
+        <div className="card-container">
+          <h4>{brand}</h4>
+          <p>{category}</p>
+          <p>
+            <strong>Rs. {price.toLocaleString()}</strong>
+          </p>
+        </div>
+        <div className="button-container">
+          <button className="add-to-cart" onClick={() => addItemsToCart()}>
+            <strong>Add to cart</strong>
+          </button>
+          <p>Available in all sizes</p>
+        </div>
+      </div>
+      {toast && (
+        <Toast
+          type="success"
+          title="Success"
+          description="Added to cart"
+          position="bottomup"
         />
       )}
-      <img
-        key={id}
-        src={photos[0]}
-        alt={brand}
-        onClick={() => setRedirect(true)}
-      />
-      <div className="card-container">
-        <h4>{brand}</h4>
-        <p>{category}</p>
-        <p>
-          <strong>Rs. {price.toLocaleString()}</strong>
-        </p>
-      </div>
-      <div className="button-container">
-        <button className="add-to-cart" onClick={() => addItemsToCart()}>
-          <strong>Add to cart</strong>
-        </button>
-        <p>Available in all sizes</p>
-      </div>
-    </div>
+    </>
   );
 };
 
